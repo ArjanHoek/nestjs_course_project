5173eac2116e3065028b101e0015ff7295e5234a
Arjan Hoek <arjanhoek5191@gmail.com>
2024-07-26 13:53:21 +0200

    create entities and set up connection with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        database: 'cars_dev',
+        password: 'postgres',
+        username: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        entities: [User, Report],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
