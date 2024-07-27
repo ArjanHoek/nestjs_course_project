f23f9e720da5ccd3bdef87e72708d33e356b5b07
Arjan Hoek <arjanhoek5191@gmail.com>
2024-07-27 20:26:05 +0200

    implement user controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto, UpdateUserDto } from './dtos';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('signup')
+  async createUser(@Body() dto: CreateUserDto) {
+    const user = await this.usersService.create(dto);
+    return { status: 'success', user };
+  }
+
+  @Get(':id')
+  public findUser(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Get()
+  find(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+    return this.usersService.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.usersService.delete(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto, UpdateUserDto } from './dtos';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  public async create({ email, password }: CreateUserDto) {
+    const hash = await argon.hash(password);
+    const user = this.repo.create({ email, hash });
+    const { id } = await this.repo.save(user);
+    return { email, id };
+  }
+
+  public async findOne(id: string) {
+    const user = await this.repo.findOne({
+      where: { id },
+      select: { id: true, email: true },
+    });
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
+  }
+
+  public find(email: string = '') {
+    return this.repo.find({
+      where: { email },
+      select: { id: true, email: true },
+    });
+  }
+
+  public async update(id: string, { password, ...dto }: UpdateUserDto) {
+    const partialUser: Partial<User> = { ...dto };
+
+    if (password) {
+      partialUser.hash = await argon.hash(password);
+    }
+
+    return this.repo.update(id, partialUser);
+  }
+
+  public delete(id: string) {
+    return this.repo.delete(id);
+  }
+}
