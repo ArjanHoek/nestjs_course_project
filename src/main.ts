bc265ed72dc141333e55abdaeb8d293bd0a9cc7a
Arjan Hoek <arjanhoek5191@gmail.com>
2024-08-09 15:23:08 +0200

    last minute updates

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const bootstrap = () =>
-  NestFactory.create(AppModule).then((app) => app.listen(3000));
+  NestFactory.create(AppModule).then((app) =>
+    app.listen(process.env.PORT || 3000),
+  );
 
 bootstrap();
