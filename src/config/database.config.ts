956aa204e1a247cdc0eca8f3196795f975f644c2
Arjan Hoek <arjanhoek5191@gmail.com>
2024-08-09 14:54:51 +0200

    add configuration options for production

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,18 +1,32 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export default registerAs('database', (): TypeOrmModuleOptions => {
-  const { DB_PASS, DB_USER, DB_HOST, DB_PORT, DB_NAME, NODE_ENV } = process.env;
-  return {
+export default registerAs('database', () => {
+  const {
+    DB_PASS,
+    DB_USER,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    NODE_ENV,
+    DATABASE_URL,
+  } = process.env;
+  const options: TypeOrmModuleOptions = {
     type: 'postgres',
     host: DB_HOST,
     port: +DB_PORT,
+    database: DB_NAME,
     username: DB_USER,
     password: DB_PASS,
-    database: DB_NAME,
+    url: DATABASE_URL,
     synchronize: false,
     entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
     migrations: [`${__dirname}/../../migrations/*{.ts,.js}`],
-    migrationsRun: NODE_ENV === 'test',
+    migrationsRun: NODE_ENV !== 'development',
+    ssl: {
+      rejectUnauthorized: false,
+    },
   };
+
+  return options;
 });
